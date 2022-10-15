0bb1800e71c84d1aede134f95f0aa5ca2561416c
🦈 <1319764245@qq.com>
2022-10-16 00:04:49 +0800

    10.15

--- _10.15_study/src/com/lianyu/arithmeticoperator/ArithmeticTest.java
+++ _10.15_study/src/com/lianyu/arithmeticoperator/ArithmeticTest.java
@@ -10,8 +10,9 @@ public class ArithmeticTest {
         int a = num % 10;       //个位
         int b = num / 10 % 10;  //十位
         int c = num / 100 % 10; //百位
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println("个位是" + a);
+        System.out.println("十位是" + b);
+        System.out.println("百位是" + c);
+
     }
 }
