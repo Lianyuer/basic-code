a7cb8bc01c8ef3dcaacc8b9965a3324d2057ff84
🦈 <1319764245@qq.com>
2022-10-15 21:32:26 +0800

    10.15

--- _10.15_study/src/com/lianyu/arithmeticoperator/ArithmeticTest.java
+++ _10.15_study/src/com/lianyu/arithmeticoperator/ArithmeticTest.java
@@ -9,7 +9,7 @@ public class ArithmeticTest {
         int num = sc.nextInt();
         int a = num % 10;       //个位
         int b = num / 10 % 10;  //十位
-        int c = num / 100 % 10;      //百位
+        int c = num / 100 % 10; //百位
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
